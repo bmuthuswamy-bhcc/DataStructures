d3ad624b578cb502d7a05f488a9d30eca7f1ebbf
bharathwaj <b.muthuswamy@bhcc.edu>
2021-02-20 13:28:01 -0500

    Add project files.

---
+++ DataStructures/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+// This is a demo of main ideas from Chapter 7 (Collections)
+
+namespace DataStructures {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
